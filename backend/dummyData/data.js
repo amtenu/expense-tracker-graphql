72c96a2e2646c6fcf3d0f18d445c9059e1ac175d
amtenu <amannov21@gmail.com>
2024-07-30 16:51:28 -0600

    added user dummy data, resolvers and typeDef

---
+++ backend/dummyData/data.js
@@ -0,0 +1,39 @@
+const users = [
+  {
+    _id: "1",
+    username: "user1",
+    name: "User One",
+    password: "password1",
+    email: " user1@gmail.com",
+  },
+  {
+    _id: "2",
+    username: "user2",
+    name: "User Two",
+    password: "password2",
+    email: " user2@gmail.com",
+  },
+  {
+    _id: "3",
+    username: "user3",
+    name: "User Three",
+    password: "password3",
+    email: "user3@gmail.com",
+  },
+  {
+    _id: "4",
+    username: "user4",
+    name: "User Four",
+    password: "password4",
+    email: " user4@gmail.com",
+  },
+  {
+    _id: "5",
+    username: "user5",
+    name: "User Five",
+    password: "password5",
+    email: " user5@gmail.com",
+  },
+];
+
+export default users;
---
+++ backend/typeDefs/user.typeDef.js
@@ -0,0 +1,39 @@
+const userTypeDef = `
+#graphql
+type User {
+  _id: ID!
+  username: String!
+  name: String!
+  profilePicture: String
+  gender: String!
+  email: String!
+}
+  
+type Query {
+users: [User!]
+authUser: User
+user(userId:ID!): User
+}
+
+type Mutation {
+signUp(input: SignUpInput!): User
+signIn(input: SignInInput!): User
+logout: LogoutResponse
+}
+
+input SignUpInput {
+username: String!'
+name: String!
+password: String!
+email: String
+}
+
+input SignInInput {
+username: String!
+password: String!
+}
+
+type LogoutResponse {
+message: String!
+}
+`;
