0e400e260c89984e5af8ac44f0b6a7424e562c22
amtenu <amannov21@gmail.com>
2024-08-13 10:24:37 -0600

    sign up mutation

--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -1,14 +1,46 @@
+import { use } from "passport";
 import { users } from "../dummyData/data.js";
+import bcrypt from "bcrypt";
 const userResolver = {
-  Query: {
-    users: () => {
-      return users;
+  Mutation: {
+    signUp: async (_, { input }, context) => {
+      try {
+        const { username, user, email, password } = input;
+        if ((username || user || email || password) === null) {
+          throw new Error("Please fill all the fields");
+        }
+        const existUser = await users.fineOne({ username: username });
+        if (existUser) {
+          throw new Error("User already exist");
+        }
+        const salt = bcrypt.genSaltSync(10);
+        const hashedPassword = bcrypt.hashSync(password, salt);
+
+        const newUser = {
+          username,
+          user,
+          email,
+          password: hash,
+        };
+
+        await newUser.save();
+        await context.login(newUser);
+
+        return newUser;
+      } catch (err) {
+        console.error("Error in signup", err);
+        throw new Error(err.message || "Internal server error");
+      }
     },
-    user: (_, { userId }) => {
-      return users.find((user) => user._id === userId);
+    Query: {
+      users: () => {
+        return users;
+      },
+      user: (_, { userId }) => {
+        return users.find((user) => user._id === userId);
+      },
     },
   },
-  Mutation: {},
 };
 
 export default userResolver;
